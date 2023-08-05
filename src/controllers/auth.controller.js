4bf68ac84fe4fc638728e339d6a3b2148d231503
thiago-arend <thiago.arend@gmail.com>
2023-08-05 16:57:59 -0300

    feat: rota POST /signin

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -8,13 +8,32 @@ export async function signup(req, res) {
     const passwordHash = bcrypt.hashSync(password, 10);
 
     try {
-
         await db.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3)`,
             [name, email, passwordHash]);
 
         res.sendStatus(201);
     } catch (err) {
-        if (Number(err.code) === 23505) return res.status(409).send({message: "E-mail já cadastrado!"});
+        if (Number(err.code) === 23505) return res.status(409).send({ message: "E-mail já cadastrado!" });
+
+        res.status(500).send(err.message);
+    }
+}
+
+export async function signin(req, res) {
+    const { email, password } = req.body; // senhas já validadas pelo schema
+
+    try {
+
+        const result = await db.query(`SELECT * FROM users WHERE email=$1`, [email]);
+        if (result.rowCount === 0 || !bcrypt.compareSync(password, result.rows[0].password))
+            return res.status(401).send({ message: "E-mail inválido ou senha incorreta!" });
+
+        const token = uuid();
+        await db.query(`INSERT INTO sessions (token, "userId") VALUES ($1, $2)`,
+            [token, result.rows[0].id]);
+
+        res.status(200).send({ token });
+    } catch (err) {
 
         res.status(500).send(err.message);
     }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { usersSignupSchema } from "../schemas/user.schemas.js";
-import { signup } from "../controllers/auth.controller.js";
+import { usersSigninSchema, usersSignupSchema } from "../schemas/user.schemas.js";
+import { signin, signup } from "../controllers/auth.controller.js";
 import validateSchema from "../middlewares/validadeSchema.js";
 
 const authRouter = Router();
 authRouter.post("/signup", validateSchema(usersSignupSchema), signup);
+authRouter.post("/signin", validateSchema(usersSigninSchema), signin);
 
 export default authRouter;
