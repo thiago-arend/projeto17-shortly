05103a0549ae04feed33fc23a10cf875ec825885
thiago-arend <thiago.arend@gmail.com>
2023-08-05 23:51:16 -0300

    feat: rota GET /urls/open/:shortUrl

--- src/controllers/url.controller.js
+++ src/controllers/url.controller.js
@@ -38,3 +38,22 @@ export async function getUrl(req, res) {
         res.status(500).send(err.message);
     }
 }
+
+export async function visitUrl(req, res) {
+    const { shortUrl } = req.params;
+
+    try {
+
+        const result = await db.query(`SELECT * FROM urls WHERE "shortUrl"=$1;`, [shortUrl]);
+        if (result.rowCount === 0) return res.status(404).send({ message: "Url encurtada não encontrada!" });
+
+        // faria mais sentido salvar também o id de quem realizou a visita...
+        await db.query(`INSERT INTO visits ("urlId") VALUES ($1);`,
+            [result.rows[0].id]);
+
+        res.redirect(`localhost:5000/${shortUrl}`);
+    } catch (err) {
+
+        res.status(500).send(err.message);
+    }
+}
--- src/routes/url.routes.js
+++ src/routes/url.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import validateSchema from "../middlewares/validadeSchema.js";
 import { urlSchema } from "../schemas/url.schema.js";
-import { createUrl, getUrl } from "../controllers/url.controller.js";
+import { createUrl, getUrl, visitUrl } from "../controllers/url.controller.js";
 import { validateAuth } from "../middlewares/validadeAuth.js";
 
 const urlRouter = Router();
 urlRouter.post("/urls/shorten", validateSchema(urlSchema), validateAuth, createUrl);
 urlRouter.get("/urls/:id", getUrl);
+urlRouter.get("/urls/open/:shortUrl", visitUrl);
 
 export default urlRouter;
