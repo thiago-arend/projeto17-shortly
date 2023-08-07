51101c508e7e94282a68c88751f63b4b94b65e21
thiago-arend <thiago.arend@gmail.com>
2023-08-07 11:51:14 -0300

    feat: rota GET /users/me

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,23 +5,19 @@ export async function getUserData(req, res) {
 
     try {
 
-        const result = await db.query(
-            `SELECT users.id, users.name, CAST (SUM(urls."visitCount") AS INTEGER) AS "visitCount", 
-            (SELECT JSON_AGG(urls.*) AS "shortenedUrls" FROM urls WHERE "creatorId"=$1)
-                    FROM users JOIN urls ON users.id=urls."creatorId"
-                    WHERE users.id=$1
-                    GROUP BY users.id;`, [userId]
+        const userInfo = await db.query(
+            `SELECT users.id, users.name, SUM(urls."visitCount") AS "visitCount"
+                FROM users JOIN urls ON users.id=urls."creatorId"
+                WHERE users.id=$1
+                GROUP BY users.id;`, [userId]
         );
 
-        const obj = result.rows[0];
-        obj.shortenedUrls = obj.shortenedUrls.map(url => {
-            delete url.creatorId;
-            delete url.createdAt;
+        const urlInfo = await db.query(
+            `SELECT id, "shortUrl", url, "visitCount"
+                FROM urls WHERE "creatorId"=$1;`, [userId]
+        );
 
-            return url;
-        });
-
-        res.status(200).send(obj);
+        res.status(200).send({ ...userInfo.rows[0], shortenedUrls: urlInfo.rows });
     } catch (err) {
 
         res.status(500).send(err.message);
